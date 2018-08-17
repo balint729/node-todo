f72077b8447c86dee3c8892ac09f881d7fa82970
Banhegyi <212714836@logon.ds.ge.com>
2018-08-17 13:04:14 +0200

    Add test DELETE/:id

--- server/server.js
+++ server/server.js
@@ -62,14 +62,14 @@ app.delete('/todos/:id', (req,res) =>{
     var id = req.params.id;
 
     if(!ObjectID.isValid(id)){
-        return res.status(400).send();
+        return res.status(404).send();
     }
 
-    Todo.findByIdAndRemove(id).then((result) =>{
-        if(!result){
-            res.status(400).send();
+    Todo.findByIdAndRemove(id).then((todo) =>{
+        if(!todo){
+            res.status(404).send();
         }
-        res.send({result});
+        res.send({todo});
 
     }).catch((e) =>{
         res.status(400).send();
