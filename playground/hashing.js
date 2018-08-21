cdc8bb6051137f974b155a84573dcba3711b533a
Banhegyi <212714836@logon.ds.ge.com>
2018-08-21 14:24:13 +0200

    Add post/users route and token method

---
+++ playground/hashing.js
@@ -0,0 +1,32 @@
+const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
+
+var data ={
+    id:10
+};
+
+var token = jwt.sign(data, '123abc');
+console.log(token);
+
+var decoded = jwt.verify(token, '123abc');
+console.log(decoded);
+// var msg = 'Hello World';
+// var hashMsg = SHA256(msg).toString();
+
+// console.log(hashMsg);
+
+// var data = {
+//     id: 4
+// };
+
+// var token = {
+//     data,
+//     hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
+// };
+
+// var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
+// if(resultHash === token.hash){
+//     console.log('Data was not changed');
+// }else{
+//     console.log('Data was changed. Do not trust!');
+// }
