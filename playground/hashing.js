c19595a76b10d90f5e5398987e0d3e9f2243c02d
Banhegyi <212714836@logon.ds.ge.com>
2018-08-22 10:40:31 +0200

    Add PW Hash

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,31 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data ={
-    id:10
-};
+var password = "asdasdasd";
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+// bcrypt.genSalt(10, (err,salt) =>{
+//     bcrypt.hash(password, salt, (error, hash) =>{
+//         console.log(hash);
+//     });
 
-var decoded = jwt.verify(token, '123abc');
-console.log(decoded);
+// });
+
+var hashedPw = "$2a$10$tIINKEB.02xx30laSkNigeU0lCxGN5ZUGAZz9MF5Z1GKFq1c0Y7uO";
+bcrypt.compare(password, hashedPw, (err, res) =>{
+    console.log(res);
+
+});
+
+// var data ={
+//     id:10
+// };
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// var decoded = jwt.verify(token, '123abc');
+// console.log(decoded);
 // var msg = 'Hello World';
 // var hashMsg = SHA256(msg).toString();
 
