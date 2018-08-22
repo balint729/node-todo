600ce3f5015fee67f692aa5e80766a6d0ec9045e
Banhegyi <212714836@logon.ds.ge.com>
2018-08-22 15:05:15 +0200

    add test for users/Login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -242,7 +242,7 @@ describe('POST /users', () =>{
         expect(user.password).toNotBe(password);
         done();
 
-      });
+      }).catch((e) => done(e));
     });
 
   });
@@ -270,6 +270,64 @@ describe('POST /users', () =>{
     .end(done);
 
 
+  });
+
+});
+
+describe('POST users/login', () =>{
+  it('should login the user and return token', (done) =>{
+    request(app)
+    .post('/users/login')
+    .send({
+      email: users[1].email,
+      password: users[1].password
+    })
+    .expect(200)
+    .expect((result) =>{
+      expect(result.headers['x-auth']).toExist();
+    }).end((err,res) =>{
+      if(err){
+        return done(err);
+      }
+
+      User.findById(users[1]._id).then((user) =>{
+        expect(user.tokens[0]).toInclude({
+          access: 'auth',
+          token: res.headers['x-auth']
+        });
+        done();
+      }).catch((e) => done(e));
+
+    })
+
+
+    
+  });
+
+  it('should reject invalid login', (done) =>{
+    request(app)
+    .post('/users/login')
+    .send({
+      email: users[1].email,
+      password: users[1].password+'1'
+    })
+    .expect(400)
+    .expect((result) =>{
+      expect(result.headers['x-auth']).toNotExist();
+    }).end((err, res) =>{
+      if(err){
+        return done(err);
+      }
+
+      User.findById(users[1]._id).then((user) =>{
+        expect(user.tokens.length).toBe(0);
+        done();
+      }).catch((e) => done(e)) ;
+
+    })
+
+
+   
   });
 
 });
