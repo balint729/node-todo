57bb255c3c8de8fcd4583a583833d004eef77c30
Banhegyi <212714836@logon.ds.ge.com>
2018-08-23 15:32:07 +0200

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,20 @@
 var env = process.env.NODE_ENV || 'development';
 
-if(env === 'development'){
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI= 'mongodb://localhost:27017/Todo';
+if(env === 'development' || env === 'test'){
+    var config = require("./config.json");
+    var envConfig = config[env];
 
-}else if(env === 'test'){
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI= 'mongodb://localhost:27017/TodoTest';
+    Object.keys(envConfig).forEach((key) =>{
+        process.env[key] = envConfig[key];
+    });
+
 }
+
+// if(env === 'development'){
+//     process.env.PORT = 3000;
+//     process.env.MONGODB_URI= 'mongodb://localhost:27017/Todo';
+
+// }else if(env === 'test'){
+//     process.env.PORT = 3000;
+//     process.env.MONGODB_URI= 'mongodb://localhost:27017/TodoTest';
+// }
